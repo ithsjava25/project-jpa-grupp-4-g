74d975d5d463c9662921f7d8cb78bf9099158357
Felix Eriksson <felix.eriksson1996@hotmail.com>
2025-12-16 13:56:23 +0100

    refactoring credits in class

--- src/main/java/org/example/player.java
+++ src/main/java/org/example/player.java
@@ -21,8 +21,12 @@ abstract class player {
         return credits;
     }
 
-    public void payCredits(long credits) {
+    public void removeCredits(long credits) {
         this.credits = this.credits - credits;
+    }
+
+    public void addCredits(long credits) {
+        this.credits = this.credits + credits;
     }
 
     void checkIfPlayerHasPenalties(){
@@ -38,23 +42,23 @@ abstract class player {
             case 1 -> {
                 System.out.println(" ");
                 System.out.println("You fell and bruised, pay some fees 50 credits");
-                payCredits(50);
+                removeCredits(50);
             } case 2 -> {
                 System.out.println(" ");
                 System.out.println("You partied to hard last night, lost 100 credits");
-                payCredits(100);
+                removeCredits(100);
             } case 3 -> {
                 System.out.println(" ");
                 System.out.println("You got scammed by a local, what a shame");
-                payCredits(150);
+                removeCredits(150);
             } case 4 -> {
                 System.out.println(" ");
                 System.out.println("You're transport broke, need a mechanic");
-                payCredits(200);
+                removeCredits(200);
             } case 5 -> {
                 System.out.println(" ");
                 System.out.println("You have a big phone bill, dont call that much");
-                payCredits(250);
+                removeCredits(250);
             } default -> {
                 System.out.println(" ");
                 System.out.println("no penalties");
