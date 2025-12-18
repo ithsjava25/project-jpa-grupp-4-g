9a7af9179af2326a8ba79bef27010985969f11d0
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2025-12-18 13:19:14 +0100

    added hibernate to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module travelGame {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
     opens org.example to javafx.fxml;
     exports org.example;
